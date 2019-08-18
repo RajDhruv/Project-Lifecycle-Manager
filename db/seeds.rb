1a962ee027f3762d8aebfd9d033fc2e443a6ce42
Dhruv Raj <rajdhruv30@gmail.com>
2019-08-19 02:01:02 +0530

    Final Changes and Seeds Deployed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |index|
+11.times do |index|
 	User.create(email:"user_#{index+1}@example.com",name:"user_#{index+1}",password:"123456",password_confirmation:"123456", roles:"developer")
 end
 
@@ -17,28 +17,36 @@ dhruv_project1 = Project.create(name:"Dhruv Project 1",description:"Description 
 puts "Dhruv Project 1 Created"
 
 dhruv_project2 = Project.create(name:"Dhruv Project 2",description:"Description of Seed Second Project",admin:dhruv, lock_version:1, developer_count:0)
-puts "Dhruv Project 1 Created"
+puts "Dhruv Project 2 Created"
 
 User.first(6).each do |user|
 	dhruv_project1.users<<user
 end
+puts "First 6 Users Mapped to First Project"
 
 dhruv_project1.update_attribute("developer_count",6)
 
 (User.last(5)-User.last(1)).each do |user|
 	dhruv_project2.users<<user
 end
+puts "Last 4 Users Mapped to Second Project"
 
 dhruv_project2.update_attribute("developer_count",4)
 
+all_user_ids=User.all.map(&:id)
+first_six=all_user_ids[1..6]
+last_six=all_user_ids[7..10]
+
 rand(18..23).times do |i|
-	user_id=rand(1..6)
+	user_id=first_six.sample
 	status=rand(0..2)
 	Task.create(description:"Project 1 Tasks Description",lock_version:1,user_id:user_id,project:dhruv_project1,status:status)
 end
+puts "Project 1 Task Allocation Completed"
 
 rand(18..23).times do |i|
-	user_id=rand(7..10)
+	user_id=last_six.sample
 	status=rand(0..2)
 	Task.create(description:"Project 1 Tasks Description",lock_version:1,user_id:user_id,project:dhruv_project2,status:status)
 end
+puts "Project 2 Task Allocation Completed"
